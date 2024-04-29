92de6345a3b1ef95f30c135e7a48fd4d5c6a6696
marquardtgustavo1 <132851768+marquardtgustavo1@users.noreply.github.com>
2024-04-29 11:07:07 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+alert("Boas vindas ao jogo do número secreto!");
+let numeroSecreto = parseInt(Math.random() *100 +1);
+console.log(numeroSecreto);
+let chute = prompt("Escolha um número de 1 a 100");
+let tentativas = 1;
+
+//enquanto o chute não for igual ao número secreto
+while (chute != numeroSecreto) {
+    chute = prompt("Escolha um número de 1 a 100");
+    //se o chute for igual ao número secreto
+    if(chute == numeroSecreto){
+        break;
+    } else {
+        if (chute > numeroSecreto) {
+            alert(`o número secreto é menor que ${chute}`) 
+        } else {
+            alert(`o número secreto é maior que ${chute}`)
+        }
+        //tentativas = tentativas + 1 
+        tentativas++
+    }
+}
+
+let palavraTentativa = tentativa > 1 ?
+'tentativas' : 'tentativas'
+alert(`Isso ai você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}`);
